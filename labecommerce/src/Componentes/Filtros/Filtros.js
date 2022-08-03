cf61b1317f1d9c8d15d23d7debb608f4257355c1
vinicius-peres <viniciusfula0409@gmail.com>
2022-08-03 16:36:23 -0300

    colocando-arquivo-de-estilizar

--- labecommerce/src/Componentes/Filtros/Filtros.js
+++ labecommerce/src/Componentes/Filtros/Filtros.js
@@ -1,32 +1,26 @@
 import React from "react";
-import styled from "styled-components";
-
-export const Container = styled.div`
- 
-border: 1px black solid;
-width: 18%;
-padding-bottom: 25%;
-margin-left: 2%;
-`
-
-export const Organizacao = styled.label`
-display: grid;
-padding: 2%;
-`
-export const Inputs = styled.input`
-width: 60%;
-`
+import {Container,Organizacao,Inputs} from './estilo'
 
 function Filtros (){
     return(
         <Container>
             <Organizacao>
                 <h4>Filtrar por:</h4>
-                <Inputs type={"text"} placeholder={"Pesquisa"} maxLength={50}/>
+                <Inputs
+                type={"text"}
+                placeholder={"Pesquisa"}
+                maxLength={50}/>
                 <br/>
-                <Inputs type={"number"} placeholder={"Valor mínimo"} min={0}/>
+                <Inputs
+                type={"number"} 
+                placeholder={"Valor mínimo"} 
+                min={0}/>
                 <br/>
-                <Inputs type={"number"} placeholder={"Valor máximo"} max={Infinity} min={0}/>
+                <Inputs 
+                type={"number"} 
+                placeholder={"Valor máximo"} 
+                max={Infinity} 
+                min={0}/>
             </Organizacao>
         </Container>
     )
