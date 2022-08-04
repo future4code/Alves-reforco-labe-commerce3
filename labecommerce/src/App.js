fe741c7893f73062371a5a37df17b7bc8044ec80
Ari Himmelstein <ahimmelstein@gmail.com>
2022-08-04 10:14:28 -0300

    ajustes

--- labecommerce/src/App.js
+++ labecommerce/src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
+import Produtos from "./components/Home/Produtos/Produtos";
+import { useState } from "react";
+import { pacoteDeProdutos } from "./pacoteDeProdutos";
 
 function App() {
+  const [ordenacao, setOrdenacao]=useState("Crescente")
+
+  
+
+  const ordenarProdutos = (event) =>{
+      setOrdenacao (event.target.value)
+  }
+
+  const adicionarProdutoNoCarrinho = (produto) =>{
+    console.log(produto)
+  }
+
   return (
     <div>
-      <p>LabEcommerce</p>
+      <Produtos 
+        quantidade = {pacoteDeProdutos.length}
+        onChangeCabecalho={ordenarProdutos}
+        ordenação = {ordenacao}
+        produtos = {pacoteDeProdutos}
+        onclick ={adicionarProdutoNoCarrinho}
+      />
     </div>
   );
 }
