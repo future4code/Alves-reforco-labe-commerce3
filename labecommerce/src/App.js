b8c624bf347758714a2674aee41f9ae915eb54f5
vinicius-peres <viniciusfula0409@gmail.com>
2022-08-04 12:53:23 -0300

    implementação-da-funcionalidade-dos-filtros

--- labecommerce/src/App.js
+++ labecommerce/src/App.js
@@ -2,32 +2,29 @@ import React from "react";
 import Produtos from "./components/Home/Produtos/Produtos";
 import { useState } from "react";
 import { pacoteDeProdutos } from "./pacoteDeProdutos";
-import Filtros from "./Componentes/Filtros/Filtros";
 
 function App() {
-  const [ordenacao, setOrdenacao]=useState("Crescente")
+  const [ordenacao, setOrdenacao] = useState("Crescente")
 
-  
 
-  const ordenarProdutos = (event) =>{
-      setOrdenacao (event.target.value)
+
+  const ordenarProdutos = (event) => {
+    setOrdenacao(event.target.value)
   }
 
-  const adicionarProdutoNoCarrinho = (produto) =>{
+  const adicionarProdutoNoCarrinho = (produto) => {
     console.log(produto)
   }
 
   return (
     <div>
-      <Produtos 
-        quantidade = {pacoteDeProdutos.length}
+      <Produtos
+        quantidade={pacoteDeProdutos.length}
         onChangeCabecalho={ordenarProdutos}
-        ordenação = {ordenacao}
-        produtos = {pacoteDeProdutos}
-        onclick ={adicionarProdutoNoCarrinho}
-      />
+        ordenação={ordenacao}
+        produtos={pacoteDeProdutos}
+        onclick={adicionarProdutoNoCarrinho} />
       <p>LabEcommerce</p>
-      <Filtros/>
     </div>
   );
 }
